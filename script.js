7638c12e10ad3a0a40bfdb466e4abc7796d7bbea
Bei Wang <bei.wang@beirilliant.com>
2017-09-28 11:43:21 +1000

    Create the funtion for player and computer to play in turn

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-    var player;
+    var huPlayer;
+    var coPlayer;
+    var players = ["X", "O"];
+    var boardArray = Array.from(Array(9).keys());
+    var cellId;
+
     $(".form-check-input").click(function() {
-        player = $(this).val();
+        huPlayer = $(this).val();
+        players.splice($.inArray(huPlayer, players), 1);
+        coPlayer = players[0];
+        console.log(coPlayer);
         $("#startMessage").css("display","none");
     });
 
     $(".cell").click(function() {
-        $(this).html(player);
+        $(this).html(huPlayer);
+        cellId = $(this).attr("id");
+        takeSpot(parseInt(cellId));
+        coMove();
     });
+
+    function takeSpot(spot) {
+        boardArray.splice($.inArray(spot, boardArray), 1);
+        console.log(boardArray);
+    }
+
+    function coMove() {
+        var randomNum = Math.floor(Math.random() * boardArray.length);
+        console.log(randomNum);
+        $("#" + boardArray[randomNum]).html(coPlayer);
+        takeSpot(boardArray[randomNum]);  
+    }
