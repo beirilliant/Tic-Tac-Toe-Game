013db6e1addb267335430c78d4d64f3d9acb9f68
Bei Wang <bei.wang@beirilliant.com>
2017-09-29 11:08:20 +1000

    Create endGame function to make code more manageable

--- script.js
+++ script.js
@@ -15,7 +15,8 @@
     ];
     var huArr = [];
     var coArr = [];
-    var endgame = 0;
+    var end = 0;
+    var message = "";
 
     $(".form-check-input").click(function() {
         huPlayer = $(this).val();
@@ -25,6 +26,9 @@
     });
 
     $(".cell").click(function() {
+        if (end == 1) {
+            boardArray = [];
+        }
         cellId = $(this).attr("id");
         if ($.inArray(parseInt(cellId), boardArray) == -1) {
             return
@@ -34,7 +38,7 @@
             console.log(huArr);   
             takeSpot(parseInt(cellId));
             winGame();
-            if (endgame == 1) {
+            if (end == 1) {
                 boardArray = [];
             }
             setTimeout(coMove, 500);
@@ -57,25 +61,26 @@
     function winGame() {
         winCombo.forEach(function(combo) {
             if (coArr.length >= 3 && $.inArray(combo[0], coArr) >= 0 && $.inArray(combo[1], coArr) >= 0 && $.inArray(combo[2], coArr) >= 0) {
-                $("#text").css("display","initial");
-                $("#text").html("The computer wins!");
-                endgame = 1;
-                setTimeout(startGame, 2000);
+                message = "Oh no! The computer wins!";
+                endGame();
             }
             if (huArr.length >= 3 && $.inArray(combo[0], huArr) >= 0 && $.inArray(combo[1], huArr) >= 0 && $.inArray(combo[2], huArr) >= 0) {
-                $("#text").css("display","initial");
-                $("#text").html("Congratulations! You win!");
-                endgame = 1;
-                setTimeout(startGame, 2000);
+                message = "Congratulations! You win!";
+                endGame();
             } else if (coArr.length + huArr.length == 9 && boardArray.length == 0) {
-                $("#text").css("display","initial");
-                $("#text").html("It is a draw!");
-                endgame = 1;
-                setTimeout(startGame, 2000);
+                message = "It is a draw!";
+                endGame();
             } else {
                 return
             }
         });
+    }
+
+    function endGame() {
+        $("#text").css("display","initial");
+        $("#text").html(message);
+        end = 1;
+        setTimeout(startGame, 2000);
     }
 
     function startGame() {
@@ -85,5 +90,6 @@
         coArr = [];
         $("#text").html("");
         $("#text").css("display","none");
-        endgame = 0;
+        end = 0;
+        message = "";
     }
